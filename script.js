a62fc7b803a13f8e29b19664626cd509ea897952
Natan Rafael <n474n0307@gmail.com>
2025-07-10 11:17:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaDtexto = document.querySelector('.resulta');
 const historicoLista = [];
-const LIMITE_DIGITOS = 11;
+const LIMITE_DIGITOS = 9;
 
 let expressao = "";
 let valorAnterior = null;
